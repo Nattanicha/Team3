897eda76ef3699b198ac2a3b63d325c3a3620540
Ratasit <si.ratasit_st@tni.ac.th>
2015-12-01 16:50:36 +0700

    calssTourist Have name,number,Type

---
+++ classTourist.py
@@ -0,0 +1,17 @@
+'''
+   
+	 class Tourist have number ,name and Type
+
+'''
+
+
+
+class Tourist(object):
+    
+	def __init__(self,number,name,Type):
+       
+		self.number = number
+       
+		self.name = name
+       
+		self.Type = Type 
