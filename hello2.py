47851617cb31e391248d8ecc0ac86c2c9760a683
Nattanicha <ea.nattanicha_st@tni.ac.th>
2015-12-08 15:45:33 +0700

    Delete hello2.py

--- hello2.py
+++
@@ -1,8 +0,0 @@
-import datetime
-now = datetime.datetime.now()
-print( now )
-months = ('-error-', 'January','February','March','April','May','June', 'July','August','September','October','November','  December')
-print( months[now.month] )
-
-for x in months:
-    print x
